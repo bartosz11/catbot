61b8e3fa729f7ec8a58be7de0f41e7b162249107
bartosz11 <86555598+bartosz11@users.noreply.github.com>
2022-10-08 19:19:21 +0200

    Bump JDA bot just crashes if I bump logback /shrug

--- src/main/java/me/bartosz1/catbot/ReadyListener.java
+++ src/main/java/me/bartosz1/catbot/ReadyListener.java
@@ -1,9 +1,10 @@
 package me.bartosz1.catbot;
 
 import me.bartosz1.catbot.commands.*;
-import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +25,7 @@ public class ReadyListener extends ListenerAdapter {
     }
 
     @Override
-    public void onReady(ReadyEvent event) {
+    public void onReady(@NotNull ReadyEvent event) {
         catbot.getBot().getShards().get(0).updateCommands()
                 .addCommands(Commands.slash("cat", "Get a cat picture."))
                 .addCommands(Commands.slash("dog", "Get a dog picture."))
